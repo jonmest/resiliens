1d1ae68f6d521ce79e324a2b3709ecd23e6a514d
Jon Cavallie Mester <jon.cavalliemester@nexergroup.com>
2022-03-08 23:00:05 +0100

    Improving CircuitBreakerException.py string representation

--- src/resiliens/circuit_breaker/CircuitBreakerException.py
+++ src/resiliens/circuit_breaker/CircuitBreakerException.py
@@ -1,16 +1,15 @@
 #  Copyright (c) 2022 - Thumos - Jon Cavallie Mester
 
+strtimeformat = "%m/%d/%Y, %H:%M:%S"
+
+
 class CircuitBreakerException(Exception):
     def __init__(self, circuit_breaker, *args):
         super(CircuitBreakerException, self).__init__(*args)
         self._circuit_breaker = circuit_breaker
 
     def __str__(self, *args, **kwargs):
-        return '[Circuit breaker: %s] Reached %d failures and will be open until' \
-               ' %s (%d sec remaining) (_last_failure: %r)' % (
-                   self._circuit_breaker.name,
-                   self._circuit_breaker.failure_count,
-                   self._circuit_breaker.open_until,
-                   round(self._circuit_breaker.open_seconds_remaining),
-                   self._circuit_breaker._last_failure,
-               )
+        return f"[Circuit breaker: {self._circuit_breaker.name}] Reached {self._circuit_breaker.failure_count}" \
+               f" failures and will be open until {self._circuit_breaker.open_until.strftime(strtimeformat)}" \
+               f" ({self._circuit_breaker.open_seconds_remaining} sec remaining)" \
+               f" (Last failure: {repr(self._circuit_breaker.last_failure)})"
